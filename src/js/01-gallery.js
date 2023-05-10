1ae066cf28ab95d0cd83754f15d1d428b16e31aa
Adam Kulik <92215997+Adashevski@users.noreply.github.com>
2023-05-10 14:45:47 +0200

    install lightbox

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,12 @@
-import { galleryItems } from "./gallery-items.js";
-// Change code below this line
-const galleryContainer = document.querySelector(".gallery");
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import { galleryItems } from './gallery-items.js';
 
-const renderGalleryItems = (items) => {
+const galleryContainer = document.querySelector('.gallery');
+
+const renderGalleryItems = items => {
   const galleryItemsHTML = items
-    .map((item) => {
+    .map(item => {
       return `
       <li class="gallery__item">
         <a class="gallery__link" href="${item.original}">
@@ -13,30 +15,30 @@ const renderGalleryItems = (items) => {
       </li>
     `;
     })
-    .join("");
+    .join('');
 
   galleryContainer.innerHTML = galleryItemsHTML;
 };
 
 renderGalleryItems(galleryItems);
 
-const galleryLinks = document.querySelectorAll(".gallery__link");
-galleryLinks.forEach((link) => {
-  link.addEventListener("click", (event) => {
+const galleryLinks = document.querySelectorAll('.gallery__link');
+galleryLinks.forEach(link => {
+  link.addEventListener('click', event => {
     event.preventDefault();
-    const source = link.querySelector(".gallery__image").dataset.source;
+    const source = link.querySelector('.gallery__image').dataset.source;
     const description =
-      link.querySelector(".gallery__image").dataset.description;
+      link.querySelector('.gallery__image').dataset.description;
     const instance = basicLightbox.create(`
       <img src="${source}" alt="${description}">
     `);
     instance.show();
-    document.addEventListener("keydown", handleKeyDown);
+    document.addEventListener('keydown', handleKeyDown);
 
     function handleKeyDown(event) {
-      if (event.code === "Escape") {
+      if (event.code === 'Escape') {
         instance.close();
-        document.removeEventListener("keydown", handleKeyDown);
+        document.removeEventListener('keydown', handleKeyDown);
       }
     }
   });
